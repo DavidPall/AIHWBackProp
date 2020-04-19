c4f513f8aa6bc70cad41b6afa86624de5f499cf2
bojthe <55790397+bojthe@users.noreply.github.com>
2020-04-19 14:14:28 +0300

    ok_tesh

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ with open("train_labels.txt") as f:
     for line in f:
         train_labels.append(int(line))
 
+
 raw_data = []
 with open("train_data.txt") as f:
     for line in f:
@@ -14,20 +15,59 @@ with open("train_data.txt") as f:
         if len(line) == 5:
             for n in line:
                 temp.append(float(n))
-        raw_data.append(temp)
+        if temp != []:
+            raw_data.append(temp)
+
 
 train_data = []
 
-for i in range(len(raw_data)//7):
+for i in range(len(train_labels)):
     temp = []
     for j in range(7):
         for num in raw_data[i*7+j]:
             temp.append(num)
     train_data.append(temp)
 
-
 train_data_array = np.array(train_data)
-train_data_array.reshape(-1,1)
 
-classifier = MLPClassifier(solver='lbfgs', alpha=1e-2, hidden_layer_sizes=(3, 4), random_state=42)
+# train_data_array.reshape(-1,1)
+
+classifier = MLPClassifier(solver='lbfgs', alpha=1e-6, hidden_layer_sizes=(3, 4), random_state=69)
 classifier.fit(train_data_array, train_labels)
+
+
+
+
+test_labels = []
+with open("test_labels.txt") as f:
+    for line in f:
+        test_labels.append(int(line))
+
+
+raw_data = []
+with open("test_data.txt") as f:
+    for line in f:
+        line = line.split()
+        temp = []
+        if len(line) == 5:
+            for n in line:
+                temp.append(float(n))
+        if temp != []:
+            raw_data.append(temp)
+
+
+test_data = []
+
+for i in range(len(test_labels)):
+    temp = []
+    for j in range(7):
+        for num in raw_data[i*7+j]:
+            temp.append(num)
+    test_data.append(temp)
+
+test_data_array = np.array(test_data)
+
+results = classifier.predict(test_data_array)
+
+print(results)
+print(test_labels)
